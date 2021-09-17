0ed5fca58e7e97c7f40cd9cb9af1fd2cfbedbc72
rbezerrajc <rbezerra@sjcc.com.br>
2021-09-17 10:50:55 -0300

    commit geral

---
+++ src/Components/User/User.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+import { UserContext } from '../../UserContext';
+import NotFound from '../NotFound';
+
+const User = () => {
+  const { data } = React.useContext(UserContext);
+
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </section>
+  );
+};
+
+export default User;
